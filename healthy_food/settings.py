9d19ae5422da8f6af8449bac6413f47421d1294b
kusdavletov <kusdavletov@gmail.com>
2019-11-28 02:04:51 +0900

    Command change

--- healthy_food/settings.py
+++ healthy_food/settings.py
@@ -112,6 +112,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "..", "www", "static"),
     'healthy_food/static/',
 ]
